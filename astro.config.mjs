05422d9bc5ba25462215011acb9d4ce87f7dedae
Mechree <michaelmc1989@gmail.com>
2026-01-19 13:13:53 -0500

    edited astro config for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://michael-mccain.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
